e2810046ccaa660224e7d3f7a4b1d62e61aa0c40
Denis Sergeev <denis-sergeev-85@inbox.ru>
2021-09-13 08:59:49 +0300

    сделано

--- script2.js
+++ script2.js
@@ -4,55 +4,55 @@ const passwordArray = document.getElementById('textArray2')
 const digitArray = document.getElementById('textArray3')
 
 function sendForm(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const login = form.querySelector('[name="login"]'),
-        password = form.querySelector('[name="password"]'),
-        digit = form.querySelector('[name="digit"]');
+  const login = form.querySelector('[name="login"]'),
+    password = form.querySelector('[name="password"]'),
+    digit = form.querySelector('[name="digit"]');
 
-    const values = {
-        login: login.value,
-        password: password.value,
-        digit: digit.value,
-    };
+  const values = {
+    login: login.value,
+    password: password.value,
+    digit: digit.value,
+  };
 
-    loginArray.innerHTML=values.login
-    passwordArray.innerHTML=values.password
-    digitArray.innerHTML=values.digit
+  loginArray.innerHTML = values.login
+  passwordArray.innerHTML = values.password
+  digitArray.innerHTML = values.digit
 
-    console.log('массив данных', values);
-    alert('данные отправлены')
-/*
-    form.onsubmit = async (e) => {
-        e.preventDefault();
-    
-        let response = await fetch('/title', {
-          method: 'POST',
-          body: new FormData(values)
-        })
-        .then(res => res.json())
-        .then(res=> console.log(res))
-    
-        console.log(result);
-    }
-*/
+  console.log('массив данных', values);
+  alert('данные отправлены')
+  /*
+      form.onsubmit = async (e) => {
+          e.preventDefault();
+      
+          let response = await fetch('/title', {
+            method: 'POST',
+            body: new FormData(values)
+          })
+          .then(res => res.json())
+          .then(res=> console.log(res))
+      
+          console.log(result);
+      }
+  */
 
-   /*
-   const sendData = async (url, data) => {
-  const response = await fetch(url, {
-    method: 'POST',
-    body: data,
-  });
+  /*
+  const sendData = async (url, data) => {
+ const response = await fetch(url, {
+   method: 'POST',
+   body: data,
+ });
 
-  if (!response.ok) {
-    throw new Error(`Ошибка по адресу ${url}, статус ошибки ${response.status}`);
-  }
+ if (!response.ok) {
+   throw new Error(`Ошибка по адресу ${url}, статус ошибки ${response.status}`);
+ }
 
-  return await response.json();
+ return await response.json();
 }
 
 
-   */
+  */
 }
 
 
